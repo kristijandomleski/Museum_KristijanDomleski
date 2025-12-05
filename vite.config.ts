ca93207e91a653aaec053454ac27f76c3dcdbe31
kristijandomleski <79118155+kristijandomleski@users.noreply.github.com>
2025-12-05 16:22:41 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/my-museum/',
+  base: '/Museum_KristijanDomleski
+/',
   plugins: [react(),tailwindcss(),],
 })
